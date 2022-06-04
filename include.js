8c2f0388c94afdc4c3102e94129b72d2a2d549cc
Hinata Ito <36560965+Rinneko-san@users.noreply.github.com>
2022-06-04 22:36:12 +0900

    Header_Complete

--- include.js
+++
@@ -1,9 +0,0 @@
-function header(){
-    $.ajax({
-        url: "parts/header.html",
-        cache: false,
-        success: function(html){
-            document.write(html);
-        }
-    });
-}
